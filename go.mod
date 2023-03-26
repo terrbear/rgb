6cf853ecea35ab87b6aaca0f8b657d7183b6cf43
Terry Heath <theath@gmail.com>
2023-03-26 15:03:24 +1300

    direct light control

--- go.mod
+++ go.mod
@@ -5,11 +5,12 @@ go 1.18
 require (
 	github.com/gorilla/websocket v1.5.0
 	github.com/kbinani/screenshot v0.0.0-20210720154843-7d3a670d8329
+	golang.org/x/net v0.8.0
 )
 
 require (
 	github.com/gen2brain/shm v0.0.0-20200228170931-49f9650110c5 // indirect
 	github.com/jezek/xgb v0.0.0-20210312150743-0e0f116e1240 // indirect
 	github.com/lxn/win v0.0.0-20210218163916-a377121e959e // indirect
-	golang.org/x/sys v0.0.0-20201018230417-eeed37f84f13 // indirect
+	golang.org/x/sys v0.6.0 // indirect
 )
--- main.go
+++ main.go
@@ -4,22 +4,16 @@ import (
 	"encoding/json"
 	"fmt"
 	"math"
+	"math/rand"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"strings"
 	"time"
 
-	"github.com/gorilla/websocket"
 	"github.com/kbinani/screenshot"
+	"golang.org/x/net/ipv4"
 )
-
-const pongWait = 10 * time.Second
-
-type Payload struct {
-	Endpoint string `json:"endpoint"`
-	Effect   string `json:"effect"`
-	Token    int64  `json:"token"`
-	Param    struct {
-		Color uint32 `json:"color"`
-	} `json:"param"`
-}
 
 func must(err error) {
 	if err != nil {
@@ -27,103 +21,158 @@ func must(err error) {
 	}
 }
 
-func main() {
-	fmt.Println("running 1.2")
+const ssdpAddress = "239.255.255.250:1982"
 
-	t := time.NewTicker(50 * time.Millisecond)
-
-	ws, _, err := websocket.DefaultDialer.Dial("wss://chromasdk.io:13339/razer/chromasdk", nil)
-	if err != nil {
-		panic(err)
-	}
-
-	if err := ws.SetReadDeadline(time.Now().Add(pongWait)); err != nil {
-		return
-	}
-	ws.SetPongHandler(func(string) error { return ws.SetReadDeadline(time.Now().Add(pongWait)) })
-
-	var app struct {
-		Title       string `json:"title"`
-		Description string `json:"description"`
-		Author      struct {
-			Name    string `json:"name"`
-			Contact string `json:"contact"`
-		} `json:"author"`
-		DeviceSupported []string `json:"device_supported"`
-		Category        string   `json:"category"`
-	}
-
-	app.Title = "terry rgb"
-	app.Description = "blah"
-	app.Author.Name = "terry"
-	app.Author.Contact = "terrbear.io"
-	app.DeviceSupported = []string{"keyboard", "chromalink", "mouse"}
-	app.Category = "application"
-
-	pinger := time.NewTicker(pongWait)
-
-	out, err := json.Marshal(app)
+func getLightAddress() string {
+	udpAddr, err := net.ResolveUDPAddr("udp4", ssdpAddress)
 	must(err)
 
-	must(ws.WriteMessage(websocket.TextMessage, out))
+	conn, err := net.ListenUDP("udp4", nil)
+	must(err)
+	defer conn.Close()
 
-	msg := make(chan uint32)
+	pConn := ipv4.NewPacketConn(conn)
+	pConn.JoinGroup(nil, udpAddr)
 
-	p := Payload{
-		Endpoint: "chromalink",
-		Effect:   "CHROMA_STATIC",
-		Token:    time.Now().UnixMilli(),
+	request, err := http.NewRequest("M-SEARCH", "*", nil)
+	must(err)
+	request.Host = ssdpAddress
+	request.Header["MAN"] = []string{`"ssdp:discover"`}
+	request.Header["ST"] = []string{"wifi_bulb"}
+	raw, err := httputil.DumpRequest(request, true)
+	must(err)
+	//fmt.Println(string(raw))
+
+	_, err = pConn.WriteTo(raw, nil, udpAddr)
+	must(err)
+
+	buffer := make([]byte, 1024)
+	n, _, _, err := pConn.ReadFrom(buffer)
+	must(err)
+	//fmt.Println(addr)
+	//fmt.Println(string(buffer[:n]))
+
+	lines := strings.Split(string(buffer[:n]), "\n")
+	for _, line := range lines {
+		if strings.HasPrefix(strings.ToLower(line), "location:") {
+			return strings.ReplaceAll(strings.TrimSpace(line)[len("location: "):], "yeelight://", "")
+		}
+		//fmt.Println("line: ", line)
 	}
+
+	return ""
+}
+
+func xmain() {
+	l := NewLight()
+
+	l.TurnOn()
+
+	time.Sleep(2 * time.Second)
+	fmt.Println("going red")
+	l.SetColor(0xFF, 0x00, 0x00)
+
+	time.Sleep(2 * time.Second)
+	fmt.Println("going green")
+	l.SetColor(0x00, 0xFF, 0x00)
+
+	time.Sleep(2 * time.Second)
+	fmt.Println("going blue")
+	l.SetColor(0x00, 0x00, 0xFF)
+
+	time.Sleep(2 * time.Second)
+	fmt.Println("bye!")
+}
+
+type message struct {
+	ID     int    `json:"id"`
+	Method string `json:"method"`
+	Params []any  `json:"params"`
+}
+
+type Light struct {
+	location string
+	msgs     chan message
+}
+
+type YeeLightMethod string
+
+const (
+	m_SetPower  YeeLightMethod = "set_power"
+	m_SetRGB    YeeLightMethod = "set_rgb"
+	m_SetBright YeeLightMethod = "set_bright"
+)
+
+func NewMessage(method YeeLightMethod, params ...any) message {
+	return message{
+		ID:     rand.Int(),
+		Method: string(method),
+		Params: params,
+	}
+}
+
+func NewLight() *Light {
+	l := &Light{
+		location: getLightAddress(),
+		msgs:     make(chan message),
+	}
+	go l.run()
+	return l
+}
+
+func (l *Light) run() {
+	conn, err := net.Dial("tcp", l.location)
+	must(err)
 
 	go func() {
 		for {
-			select {
-			case <-pinger.C:
-				ws.SetWriteDeadline(time.Now().Add(50 * time.Millisecond))
-				must(ws.WriteMessage(websocket.PingMessage, []byte("ping")))
-			case m := <-msg:
-				if m == p.Param.Color {
-					continue
-				}
+			buffer := make([]byte, 1024)
+			n, err := conn.Read(buffer)
+			must(err)
 
-				p.Param.Color = m
-
-				must(ws.SetWriteDeadline(time.Now().Add(500 * time.Millisecond)))
-
-				out, err := json.Marshal(p)
-				must(err)
-
-				must(ws.WriteMessage(websocket.TextMessage, out))
-			}
+			fmt.Println("reply: ", string(buffer[:n]))
 		}
 	}()
 
-	/*
-		for i := uint32(0xFF); i < 0xFF000000; i <<= 1 {
-			fmt.Printf("color: %b\n", i)
-			msg <- i
-			time.Sleep(3 * time.Second)
-		}
+	encoder := json.NewEncoder(conn)
 
-		fmt.Printf("going blue %d (%b)\n", rgb2bgr(0x0c), rgb2bgr(0x0c))
-		msg <- rgb2bgr(0x0c)
-		msg <- 0xFF000000
-		time.Sleep(3 * time.Second)
+	for msg := range l.msgs {
+		must(encoder.Encode(msg))
+		_, err := conn.Write([]byte("\r\n"))
+		must(err)
+	}
 
-		fmt.Printf("going red %d (%b)\n", rgb2bgr(0xc0), rgb2bgr(0xc0))
-		msg <- rgb2bgr(0xc0)
-		time.Sleep(3 * time.Second)
+}
 
-		fmt.Printf("going green %d (%b)\n", rgb2bgr(0x30), rgb2bgr(0x30))
-		msg <- rgb2bgr(0x30)
-		time.Sleep(3 * time.Second)
-	*/
+func (l *Light) TurnOn() {
+	l.msgs <- NewMessage(m_SetPower, "on", "smooth", 500)
+}
 
-	errcount := 0
+func (l *Light) SetColor(red, green, blue int) {
+	//fmt.Println("setting color to: ", red, green, blue)
+	rgb := red<<16 | green<<8 | blue
+	l.msgs <- NewMessage(m_SetRGB, rgb, "smooth", 500)
+}
+
+func (l *Light) SetBrightness(brightness int) {
+	//fmt.Println("setting brightness to: ", brightness)
+	l.msgs <- NewMessage(m_SetBright, brightness, "smooth", 500)
+}
+
+func main() {
+	fmt.Println("running 2.0")
+
+	l := NewLight()
+	l.TurnOn()
+
+	t := time.NewTicker(100 * time.Millisecond)
 
 	samples := make(map[int]uint8)
+	errcount := 0
 
 	for {
+		<-t.C
+
 		bounds := screenshot.GetDisplayBounds(0)
 
 		img, err := screenshot.CaptureRect(bounds)
@@ -145,6 +194,7 @@ func main() {
 			}
 		} else {
 			changed := false
+			//fmt.Println("checking samples")
 			for i, v := range samples {
 				if img.Pix[i] != v {
 					changed = true
@@ -152,32 +202,28 @@ func main() {
 				}
 			}
 			if !changed {
+				//fmt.Println("nothing to do")
 				continue
 			}
 		}
 
-		for i, p := range img.Pix {
-			component := rgba[i%4]
+		for i := 0; i < len(img.Pix); i += 4 {
+			r, g, b, a := img.Pix[i], img.Pix[i+1], img.Pix[i+2], img.Pix[i+3]
 			pixels++
-			rgba[i%4] = component*(1-(1/pixels)) + float64(p)*(1/pixels)
+			rgba[0] = rgba[0]*(1-(1/pixels)) + float64(r)*(1/pixels)*(float64(a)/0xFF)
+			rgba[1] = rgba[1]*(1-(1/pixels)) + float64(g)*(1/pixels)*(float64(a)/0xFF)
+			rgba[2] = rgba[2]*(1-(1/pixels)) + float64(b)*(1/pixels)*(float64(a)/0xFF)
+			rgba[3] = rgba[3]*(1-(1/pixels)) + float64(a)*(1/pixels)
 		}
 
 		red := 0xFF & uint32(math.Round(rgba[0]))
 		green := 0xFF & uint32(math.Round(rgba[1]))
 		blue := 0xFF & uint32(math.Round(rgba[2]))
 
-		if red > green && red > blue {
-			red |= 0x80
-		} else if green > red && green > blue {
-			green |= 0x80
-		} else if blue > red && blue > green {
-			blue |= 0x80
-		}
-
-		bgr := red | green<<8 | blue<<16
-
-		msg <- bgr
+		l.SetColor(int(red), int(green), int(blue))
+		brightness := int(float64(red+green+blue) / (3 * 0xFF) * 100)
+		//fmt.Println("brightness avg: ", brightness)
+		l.SetBrightness(brightness)
 		errcount = 0
-		<-t.C
 	}
 }
